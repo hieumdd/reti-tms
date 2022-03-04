9d2c7917149724b625697f079cfa9af3501df878
HM <hieumdd@gmail.com>
2022-03-04 23:15:08 +0700

    feat: Added task_service

---
+++ tasks/tasks_service.py
@@ -0,0 +1,18 @@
+from tms import pipeline
+from tasks import cloud_tasks
+
+
+def task_service(body: dict[str, str]):
+    return {
+        "tasks": cloud_tasks.create_tasks(
+            [
+                {
+                    "table": i,
+                    "start": body.get("start"),
+                    "end": body.get("end"),
+                }
+                for i in pipeline.pipelines.keys()
+            ],
+            lambda x: x["table"],
+        )
+    }
